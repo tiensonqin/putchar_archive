0c9e379ca965242b68a0c03fb21c3dcc5175e5b2
Tienson Qin <tiensonqin@gmail.com>
2018-08-16 07:47:03 +0800

    fixed issue of corrupted message.

--- backend/src/ssr/middleware/auth.clj
+++ backend/src/ssr/middleware/auth.clj
@@ -7,6 +7,7 @@
             [clojure.java.jdbc :as j]
             [ring.util.response :as resp]
             [api.services.slack :as slack]
+            [api.config :refer [config]]
             ;; [clojure.repl]
             [clojure.set :as set]
             [share.routes :refer [login-routes]]
@@ -28,28 +29,32 @@
 (defn wrap-auth
   [handler]
   (fn [req]
-    (let [tokens (cookie/get-token req)
-          {:keys [access-token refresh-token]} tokens]
-      (j/with-db-connection [conn (get-in req [:context :datasource])]
-        (if access-token
-         (try
-           (let [user (jwt/unsign access-token)
-                 uid (some-> (:id user) util/->uuid)]
-             (when uid (u/update conn uid {:last_seen_at (du/sql-now)}))
-             (-> (assoc-in req [:context :uid] uid)
-                 (handler)))
-           (catch Exception e
-             (if (= :exp (:cause (ex-data e)))
-               (let [user (jwt/unsign-skip-validation access-token)
-                     uid (some-> (:id user) util/->uuid)]
-                 (-> (assoc-in req [:context :uid] uid)
-                     (handler)
-                     (assoc :cookies (u/generate-tokens conn {:id uid}))))
-               (do
-                 (slack/error e)
-                 (resp/redirect "/error.html")))))
+    (try
+      (let [tokens (cookie/get-token req)
+           {:keys [access-token refresh-token]} tokens]
+       (j/with-db-connection [conn (get-in req [:context :datasource])]
+         (if access-token
+           (try
+             (let [user (jwt/unsign access-token)
+                   uid (some-> (:id user) util/->uuid)]
+               (when uid (u/update conn uid {:last_seen_at (du/sql-now)}))
+               (-> (assoc-in req [:context :uid] uid)
+                   (handler)))
+             (catch Exception e
+               (if (= :exp (:cause (ex-data e)))
+                 (let [user (jwt/unsign-skip-validation access-token)
+                       uid (some-> (:id user) util/->uuid)]
+                   (-> (assoc-in req [:context :uid] uid)
+                       (handler)
+                       (assoc :cookies (u/generate-tokens conn {:id uid}))))
+                 (do
+                   (slack/error e)
+                   (resp/redirect "/error.html")))))
 
-         (if (auth-blocked? req)
-           {:status 401
-            :body {:message "unauthorized"}}
-           (handler req)))))))
+           (if (auth-blocked? req)
+             {:status 401
+              :body {:message "unauthorized"}}
+             (handler req)))))
+      (catch Exception e
+        (-> (resp/redirect (:website-uri config))
+            (assoc :cookies cookie/delete-token))))))
